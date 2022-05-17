7b1191604a3d74a2fc79988cd2a43b8cbc233bda
MATHEUS MAKOTO KODAMA <matheus.mkodama@senacsp.edu.br>
2022-05-17 16:04:27 -0300

    auth

---
+++ DevSocial/js/auth.js
@@ -0,0 +1,16 @@
+
+
+
+function register(){
+    let name = document.getElementById('name');
+    let phone = document.getElementById('phone');
+    let cpf = document.getElementById('cpf');
+    let email = document.getElementById('email');
+    let pw = document.getElementById('pw');
+    let cpw = document.getElementById('cpw');
+
+    if(pw!=cpw){
+        alert('error');
+    }
+
+}
