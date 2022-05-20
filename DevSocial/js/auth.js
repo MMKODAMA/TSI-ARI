0ed50a3d0394df374f11f7c94167a7908cd85a98
MATHEUS MAKOTO KODAMA <matheus.mkodama@senacsp.edu.br>
2022-05-20 19:03:58 -0300

    auth

--- DevSocial/js/auth.js
+++ DevSocial/js/auth.js
@@ -1,16 +1,40 @@
-
-
-
-function register(){
     let name = document.getElementById('name');
     let phone = document.getElementById('phone');
     let cpf = document.getElementById('cpf');
     let email = document.getElementById('email');
     let pw = document.getElementById('pw');
     let cpw = document.getElementById('cpw');
+  
+  
+  function User(name, phone, cpf, email, password) {
+    this.name = name;
+    this.phone = phone;
+    this.cpf = cpf;
+    this.email=email;
+    this.password = password;
+  }
 
-    if(pw!=cpw){
+function register(){
+
+    if(pw.value==cpw.value){
+        alert('pw = cpw');
+        let novoUsuario = new User(name,phone,cpf,email,pw);
+        
+        window.localStorage.setItem('Users',JSON.stringify(novoUsuario));
+    }else{
+        console.log(cpw.value);
+        console.log(pw.value);
         alert('error');
+        
     }
-
+    
 }
+        cpw.addEventListener('onkeydown', (event)=>{
+            event.preventDefault();
+            if(cpw.value!=pw.value){
+                alert('error');
+                cpw.style.backgroundColor=red;
+            }else{
+                cpw.style.backgroundColor=green;
+            }
+        })
