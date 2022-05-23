559353e13579063577f25bf43027915e89f854a6
Makoto <matheus.mkodama@gmail.com>
2022-05-23 17:00:45 -0300

    localstorage user

--- DevSocial/js/auth.js
+++ DevSocial/js/auth.js
@@ -1,40 +1,48 @@
-    let name = document.getElementById('name');
-    let phone = document.getElementById('phone');
-    let cpf = document.getElementById('cpf');
-    let email = document.getElementById('email');
-    let pw = document.getElementById('pw');
-    let cpw = document.getElementById('cpw');
-  
-  
-  function User(name, phone, cpf, email, password) {
-    this.name = name;
-    this.phone = phone;
-    this.cpf = cpf;
-    this.email=email;
-    this.password = password;
-  }
+let nome = document.getElementById('name');
+let phone = document.getElementById('phone');
+let cpf = document.getElementById('cpf');
+let email = document.getElementById('email');
+let pw = document.getElementById('pw');
+let cpw = document.getElementById('cpw');
 
-function register(){
 
-    if(pw.value==cpw.value){
+
+
+function register() {
+    let user = {
+        name: nome.value,
+        phone: phone.value,
+        cpf: cpf.value,
+        email: email.value,
+        password: pw.value
+    }
+    if (pw.value == cpw.value) {
         alert('pw = cpw');
-        let novoUsuario = new User(name,phone,cpf,email,pw);
-        
-        window.localStorage.setItem('Users',JSON.stringify(novoUsuario));
-    }else{
+
+        let novoUsuario = user;
+
+        window.localStorage.setItem('Users', JSON.stringify(novoUsuario));
+    } else {
         console.log(cpw.value);
         console.log(pw.value);
         alert('error');
-        
     }
-    
+
 }
-        cpw.addEventListener('onkeydown', (event)=>{
-            event.preventDefault();
-            if(cpw.value!=pw.value){
-                alert('error');
-                cpw.style.backgroundColor=red;
-            }else{
-                cpw.style.backgroundColor=green;
-            }
-        })
+
+function login(){
+    let loginEmail = document.getElementById('loginEmail');
+    let loginPassword = document.getElementById('loginPassword');
+
+    window.localStorage.getItem('Users');
+
+}
+cpw.addEventListener('onkeydown', (event) => {
+    event.preventDefault();
+    if (cpw.value != pw.value) {
+        cpw.style.backgroundColor = red;
+    } else {
+        cpw.style.backgroundColor = green;
+    }
+});
+
